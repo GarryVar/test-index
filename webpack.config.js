c0c5a9240a491354b38b1afda89c9d95667ab487
GarryVar <darisvet.sokolov@yandex.ru>
2020-10-29 12:31:34 +0300

    Added babel to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,20 @@ module.export = {
   output: {
     path: path.resolve(__dirname, 'dist/scripts'),
     filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 }
