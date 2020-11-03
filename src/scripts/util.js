b4e78ce4eaf84b4ddf776ac7b953a6dd2526b9e1
GarryVar <darisvet.sokolov@yandex.ru>
2020-11-03 14:34:58 +0300

    Added intro styles

--- src/scripts/util.js
+++ src/scripts/util.js
@@ -6,12 +6,14 @@
     this.toggleHandler();
   },
 
+
   onToggle: function(elem, elemClass) {
     elem.classList.contains(elemClass) ? elem.classList.remove(elemClass) : elem.classList.add(elemClass);
   },
 
+
   toggleHandler: function() {
-    this.toggleBtn.addEventListener('click', () => {
+    this.toggleBtn.addEventListener('click', evt => {
       this.onToggle(this.nav, 'nav--hidden');
       for (let i = 0; i < this.toggleBtn.children.length; i++) {
         this.onToggle(this.toggleBtn.children[i], 'toggle__shape--hidden')
@@ -20,3 +22,19 @@
   }
 };
 
+
+
+class Character {
+  constructor(name, id) {
+    this.name = name;
+    this.id = id;
+  }
+};
+const john = new Character('John', 'OKm4ntioc0F8K');
+
+john.foo = () => this.name + this.id;
+
+
+for(let key in john) {
+  console.log(key.split('.'));
+}
