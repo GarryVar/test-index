d481a3bde9ea5f7ad664f7e40acd74848c288716
GarryVar <darisvet.sokolov@yandex.ru>
2020-11-11 19:56:53 +0300

    Added some scripts

---
+++ src/scripts/callbacks.js
@@ -0,0 +1,6 @@
+export const array = [
+  {name: 'Igor', id: 'GDJ47rgfN'},
+  {name: 'Sam', id: 'M847ghV20'},
+  {name: 'James', id: 'V4848HDL9'},
+  {name: 'Edward', id: 'JFDgk915M'},
+];
--- src/scripts/util.js
+++ src/scripts/util.js
@@ -13,28 +13,15 @@
 
 
   toggleHandler: function() {
-    this.toggleBtn.addEventListener('click', evt => {
+    this.toggleBtn.addEventListener('click', () => {
+      console.log(this);
+      // Показывает и прячет навигацию
       this.onToggle(this.nav, 'nav--hidden');
+
+      // Меняет иконку с бургера на крестик при нажатии
       for (let i = 0; i < this.toggleBtn.children.length; i++) {
         this.onToggle(this.toggleBtn.children[i], 'toggle__shape--hidden')
       }
     })
   }
 };
-
-
-
-class Character {
-  constructor(name, id) {
-    this.name = name;
-    this.id = id;
-  }
-};
-const john = new Character('John', 'OKm4ntioc0F8K');
-
-john.foo = () => this.name + this.id;
-
-
-for(let key in john) {
-  console.log(key.split('.'));
-}
